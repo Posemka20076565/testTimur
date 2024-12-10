6d34866246811ff6df1a83109781957966386c96
Timur2008git <bebex21031972@gmail.com>
2024-12-10 20:53:23 +0200

    test

--- QA/src/main/java/org/example/GitHub_Login_Test.java
+++ QA/src/main/java/org/example/GitHub_Login_Test.java
@@ -1,9 +1,8 @@
-package org.example.GitHub.Log_in;
+package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GitHub_Login_Test {
